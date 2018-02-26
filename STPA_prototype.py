d6ea39bd20b7f7a19410bc9f27cc58db5f4d6382
unknown <nikert_ludvik@hotmail.com>
2018-02-26 15:31:09 +0100

    mostly working

--- STPA_prototype.py
+++ STPA_prototype.py
@@ -19,19 +19,20 @@ class Todo(db.Model):
     def __init__(self, title, text):
         self.title = title
         self.text = text
-        self.done = False
+        self.vcs_check = False
         self.pub_date = datetime.utcnow()
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
 def index():
     return render_template('index.html',
-                           todos=Todo.query.order_by(Todo.pub_date.desc()).all()
+                           todos=Todo.query.order_by(Todo.id.asc()).all()
                            )
+
+
+@app.route('/hello')
+def hello_world():
+    return 'Hello!!!! Werld!'
 
 
 @app.route('/new', methods=['GET', 'POST'])
@@ -42,6 +43,18 @@ def new():
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('new.html')
+
+
+@app.route('/todos/<int:todo_id>', methods=['GET', 'POST'])
+def show_or_update(todo_id):
+    todo_item = Todo.query.get(todo_id)
+    if request.method == 'GET':
+        return render_template('view.html',todo=todo_item)
+    todo_item.title = request.form['title']
+    todo_item.text = request.form['text']
+    todo_item.vcs_check = ('bcs_check.%d' % todo_id) in request.form
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run()
