b5b36a525378e21fd4a3c3aa5a01fe0428f72ffc
unknown <nikert_ludvik@hotmail.com>
2018-04-09 12:23:06 +0200

    working register user

--- stpa_prototype/auth/auth.py
+++ stpa_prototype/auth/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, url_for, redirect
-# from stpa_prototype import user_datastore
+from stpa_prototype.database import db_session
 import stpa_prototype
 
 auth_blueprint = Blueprint('auth', __name__, template_folder='templates', url_prefix='/auth')
@@ -9,16 +9,18 @@ auth_blueprint = Blueprint('auth', __name__, template_folder='templates', url_pr
 def register():
     if request.method == 'POST':
         stpa_prototype.user_datastore.create_user(email=request.form['email'], password=request.form['password'])
+        db_session.commit()
         return redirect(url_for('auth.login'))
     return render_template('auth/register.html')
 
 
-# @auth_blueprint.route('/login', methods=['GET', 'POST'])
-# def register():
-#     if request.method == 'POST':
-#         # stpa_prototype.user_datastore.create_user(email=request.form['email'], password=request.form['password'])
-#         return redirect(url_for('goals.index'))
-#     return render_template('auth/login.html')
+@auth_blueprint.route('/login', methods=['GET', 'POST'])
+def login():
+    return 'Hello!!!! Werld!'
+    # if request.method == 'POST':
+    #     # stpa_prototype.user_datastore.create_user(email=request.form['email'], password=request.form['password'])
+    #     return redirect(url_for('goals.index'))
+    # return render_template('auth/login.html')
 
 
 
